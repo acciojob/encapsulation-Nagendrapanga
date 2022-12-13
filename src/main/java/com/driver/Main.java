7b2ba19993e611849a928d3bf5843db7120be4c9
Nagendrapanga <nagendrapanga1069@gmail.com>
2022-12-13 13:13:33 +0530

    encap

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+    public static void main(String [] args){
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Nagendra"
+        //'name' has private access in 'com.driver.RWOnly'
+        obj.setName("rouch");
+        System.out.println(obj.getName());
+    }
+
 }
